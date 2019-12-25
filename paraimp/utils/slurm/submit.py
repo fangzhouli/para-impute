e8a195649cb06321ee3afc7b33e98b75245976da
fangzhouli <fzli0805@gmail.com>
2019-12-25 10:13:31 +0000

    ignore

--- paraimp/utils/slurm/submit.py
+++ paraimp/utils/slurm/submit.py
@@ -1,15 +1,24 @@
 import subprocess
 
+from .const import SchedulerConst
+
 class Scheduler(object):
 
-    def __init__(self, n_nodes = 1, n_cores = 1, n_features = 1,
-            partition = None, memory = 0, time = 0):
+    def __init__(self, n_nodes = 1, n_cores = 1, partition = None, memory = 0,
+            time = 0):
         self.n_nodes    = n_nodes
         self.n_cores    = n_cores
-        self.n_features = n_features
         self.partition  = partition
         self.memory     = memory
         self.time       = time
+
+    def get_call_command(self):
+        return [
+            SchedulerConst.FLAG_NODE,
+            self.n_nodes,
+            SchedulerConst.FLAG_CORE,
+            self.n_cores,
+            SchedulerConst.]
 
     def submit(self, args_list):
         # arg[1] : path to the script file
